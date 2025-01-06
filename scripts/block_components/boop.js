2b5c3b7e832c2cf41cb0afab7e26f57042963c2c
Hog <64306705+W3rtJack@users.noreply.github.com>
2025-01-06 03:40:07 +0000

    Delete scripts directory

--- scripts/block_components/boop.js
+++
@@ -1,9 +0,0 @@
-import { world } from "@minecraft/server";
-
-export const boop = {
-    onPlayerInteract(e) {
-        const { block } = e;
-
-        world.playSound("random.boop",block.location)
-    }
-}
